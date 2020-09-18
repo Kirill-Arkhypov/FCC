a7f438bb5cd02afe8c175aa10cdce229991ff724
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-09-18 22:20:48 +0300

    Markdown Previewer: finished functionality

--- 3. Front End Libraries/2. Markdown Previewer/src/index.js
+++ 3. Front End Libraries/2. Markdown Previewer/src/index.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import marked from 'marked';
+
+import placeholder from './placeholder.js';
 import './index.css';
 
-ReactDOM.render(<React.StrictMode></React.StrictMode>, document.getElementById('root'));
+function App() {
+  const [input, parsedInput] = useState(placeholder);
+
+  return (
+    <div id='container'>
+      <textarea id='editor' onChange={(e) => parsedInput(e.target.value)}>
+        {input}
+      </textarea>
+      <div
+        id='preview'
+        dangerouslySetInnerHTML={{ __html: marked(input, { breaks: true }) }}
+      ></div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
