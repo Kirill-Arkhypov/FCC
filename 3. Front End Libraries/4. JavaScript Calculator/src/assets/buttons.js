bd6c8729dd898e3e9dd6ef4af7276f061fb0a782
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-09-27 16:27:11 +0300

    Javascript Calculator: buttons file minor fix

--- 3. Front End Libraries/4. JavaScript Calculator/src/assets/buttons.js
+++ 3. Front End Libraries/4. JavaScript Calculator/src/assets/buttons.js
@@ -5,43 +5,43 @@ const buttons = [
   },
   {
     id: 'zero',
-    value: 0,
+    value: '0',
   },
   {
     id: 'one',
-    value: 1,
+    value: '1',
   },
   {
     id: 'two',
-    value: 2,
+    value: '2',
   },
   {
     id: 'three',
-    value: 3,
+    value: '3',
   },
   {
     id: 'four',
-    value: 4,
+    value: '4',
   },
   {
     id: 'five',
-    value: 5,
+    value: '5',
   },
   {
     id: 'six',
-    value: 6,
+    value: '6',
   },
   {
     id: 'seven',
-    value: 7,
+    value: '7',
   },
   {
     id: 'eight',
-    value: 8,
+    value: '8',
   },
   {
     id: 'nine',
-    value: 9,
+    value: '9',
   },
   {
     id: 'decimal',
