534a2a84817db1dda714d36433550c3bb9577706
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-09-29 10:22:53 +0300

    Javascript Calculator: added computational logc

---
+++ 3. Front End Libraries/4. JavaScript Calculator/src/assets/computationalLogic.js
@@ -0,0 +1,29 @@
+const compute = (current, previous, operator) => {
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = previous + current;
+      break;
+
+    case '-':
+      result = result = previous - current;
+      break;
+
+    case '*':
+      result = result = previous * current;
+      break;
+
+    case '/':
+      result = result = previous / current;
+      break;
+
+    default:
+      return;
+  }
+
+  result = Math.round(1000000000000 * result) / 1000000000000;
+  return result.toString();
+};
+
+export default compute;
