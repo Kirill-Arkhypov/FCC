6adfddd24b5cc9c06b4d890b08cb57c0b9f6b967
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-10-04 12:24:47 +0300

    Heat Map: final version

--- 4. Data Visualization/3. Heat Map/script.js
+++ 4. Data Visualization/3. Heat Map/script.js
@@ -1,6 +1,6 @@
-const width = 1400;
-const height = 600;
-const { paddingX, paddingY } = { paddingX: 65, paddingY: 30 };
+const width = 1350;
+const height = 500;
+const margin = { top: 5, right: 30, bottom: 100, left: 90 };
 
 const dataSet =
   'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
@@ -21,22 +21,28 @@ const months = [
 ];
 
 const colors = [
+  '#313695',
   '#4575b4',
   '#74add1',
   '#abd9e9',
-  '#e0f3f8',
   '#ffffbf',
   '#fee090',
   '#fdae61',
   '#f46d43',
   '#d73027',
+  '#a50026',
+  '#7c001b',
 ];
+
+// Main canvas
 
 const svg = d3
   .select('#root')
   .append('svg')
   .attr('width', width)
   .attr('height', height);
+
+// Tooltip
 
 const tooltip = d3
   .select('#root')
@@ -45,41 +51,47 @@ const tooltip = d3
   .style('position', 'absolute')
   .style('visibility', 'hidden');
 
-function tooltipTemplate(year, month, baseTemperature, variance) {
+const round = (num) => Math.round(num * 100) / 100;
+
+function tooltipTemplate(year, month, base, variance) {
   return `<p>${year} - ${months[month - 1]}</p>
-      <p>${baseTemperature - variance}</p>
-      <p>${variance}</p>`;
+          <p>${round(base + variance)}℃</p>
+          <p>${round(variance)}℃</p> `;
 }
 
-d3.json(dataSet).then(({ baseTemperature, monthlyVariance }) => {
-  const cellWidth = width / (monthlyVariance.length / 12);
-  const cellHeight = (height - paddingY * 2) / months.length;
+// Fetching and parsing data
 
-  const [minYear, maxYear] = d3.extent(monthlyVariance, (d) => +d.year);
+d3.json(dataSet).then(({ baseTemperature, monthlyVariance }) => {
+  const data = monthlyVariance.map((e) => {
+    return { ...e, temperature: baseTemperature + e.variance };
+  });
+
+  // Description rendering
+
+  const [minYear, maxYear] = d3.extent(data, (d) => d.year);
 
   d3.select('#description').html(
     `<h3>${minYear} - ${maxYear}: base temperature ${baseTemperature}℃</h3>`
   );
 
+  // Scales
+
   const xScale = d3
     .scaleLinear()
     .domain([minYear - 0.25, maxYear])
-    .range([paddingX, width - paddingX]);
+    .range([margin.left, width - margin.right]);
 
   const yScale = d3
     .scaleBand()
     .domain(months)
-    .range([paddingY, height - paddingY]);
+    .range([margin.top, height - margin.bottom]);
 
-  // const colorScale = d3
-  //   .scaleLinear()
-  //   // .domain([1, 20])
-  //   .domain(d3.extent(monthlyVariance, (d) => baseTemperature - d.variance))
-  //   .range(['white', 'orange']);
   const colorScale = d3
-    .scaleBand()
-    .domain(colors)
+    .scaleLinear()
+    .domain(d3.extent(data, (d) => d.temperature))
     .range([0, colors.length - 1]);
+
+  // Axes
 
   const xAxis = d3
     .axisBottom(xScale)
@@ -90,7 +102,7 @@ d3.json(dataSet).then(({ baseTemperature, monthlyVariance }) => {
   svg
     .append('g')
     .attr('id', 'x-axis')
-    .attr('transform', `translate(0, ${height - paddingY})`)
+    .attr('transform', `translate(0, ${height - margin.bottom})`)
     .call(xAxis);
 
   const yAxis = d3.axisLeft(yScale).tickSize(10).tickSizeOuter(0);
@@ -98,14 +110,15 @@ d3.json(dataSet).then(({ baseTemperature, monthlyVariance }) => {
   svg
     .append('g')
     .attr('id', 'y-axis')
-    .attr('transform', `translate(${paddingX}, 0)`)
+    .attr('transform', `translate(${margin.left}, 0)`)
     .call(yAxis);
 
-  const cell = svg
-    .selectAll('rect')
-    .data(monthlyVariance)
-    .enter()
-    .append('rect');
+  // Data cells
+
+  const cellWidth = width / (monthlyVariance.length / 12);
+  const cellHeight = (height - margin.top - margin.bottom) / months.length;
+
+  const cell = svg.selectAll('rect').data(data).enter().append('rect');
 
   cell
     .attr('x', (d) => xScale(d.year))
@@ -114,29 +127,79 @@ d3.json(dataSet).then(({ baseTemperature, monthlyVariance }) => {
     .attr('height', cellHeight)
     .attr('data-month', (d) => d.month - 1)
     .attr('data-year', (d) => d.year)
-    .attr('data-temp', (d) => baseTemperature - d.variance)
-    // .attr('fill', (d) => {
-    //   // console.log(colorScale(colors[Math.round(d.variance)]));
-    //   return colorScale(baseTemperature - d.variance * 2);
-    // })
-    .attr(
-      'fill',
-      // (d) => colors[Math.round(colorScale(baseTemperature - d.variance))]
-      (d) => {
-        console.log(colorScale(colors[d.variance]));
-        return colors[colorScale(Math.floor(baseTemperature - d.variance))];
-      }
-    )
-    .attr('class', 'cell')
+    .attr('data-temp', (d) => d.temperature.toPrecision(3))
+    .attr('fill', (d) => colors[Math.floor(colorScale(d.temperature))])
+    .attr('class', 'cell');
+
+  // Show tooltip on cell hover
+
+  cell
     .on('mouseover', (e, d) => {
+      const x = +e.currentTarget.getAttribute('x');
+      const y = +e.currentTarget.getAttribute('y');
+
       tooltip
         .attr('data-year', d.year)
-        .style('top', () => +e.currentTarget.getAttribute('y') - 35 + 'px')
-        .style('left', () => +e.currentTarget.getAttribute('x') + 10 + 'px')
+        .style('top', () => {
+          if (y < 80) {
+            return y + 35 + 'px';
+          }
+          return y - 74 + 'px';
+        })
+        .style('left', () => {
+          if (x > 1250) {
+            return x - 155 + 'px';
+          } else if (x < 175) {
+            return x + 5 + 'px';
+          }
+          return x - 76 + 'px';
+        })
         .style('visibility', 'visible')
         .html(tooltipTemplate(d.year, d.month, baseTemperature, d.variance));
     })
     .on('mouseout', () => {
       tooltip.style('visibility', 'hidden');
     });
+
+  // Legend
+
+  const legendWidth = 500;
+  const legendHeight = 30;
+  const rectWidth = legendWidth / colors.length;
+
+  const legend = svg
+    .append('g')
+    .attr('id', 'legend')
+    .attr('width', legendWidth)
+    .attr('height', legendHeight)
+    .attr(
+      'transform',
+      `translate(${margin.left}, ${height - margin.bottom + 45})`
+    );
+
+  legend
+    .selectAll('rect')
+    .data(colors)
+    .enter()
+    .append('rect')
+    .attr('x', (d, i) => i * rectWidth)
+    .attr('y', 0)
+    .attr('width', rectWidth)
+    .attr('height', legendHeight)
+    .attr('fill', (d) => d);
+
+  // Legend axis
+
+  const legendScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, (d) => d.variance))
+    .range([0, legendWidth]);
+
+  const legendAxis = d3.axisBottom(legendScale).tickSizeOuter(0);
+
+  legend
+    .append('g')
+    .attr('id', 'legend-axis')
+    .attr('transform', `translate(0, ${legendHeight})`)
+    .call(legendAxis);
 });
