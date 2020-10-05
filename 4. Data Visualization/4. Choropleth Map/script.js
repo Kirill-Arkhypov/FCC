f3b2c9ec48802c8c991ab2e0aa2f38a1a0188999
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-10-05 22:18:33 +0300

    Choropleth Map: raw version

--- 4. Data Visualization/4. Choropleth Map/script.js
+++ 4. Data Visualization/4. Choropleth Map/script.js
@@ -1,6 +1,14 @@
-const width = 1400;
+const width = 960;
 const height = 600;
-const margin = { top: 5, right: 30, bottom: 100, left: 90 };
+
+const path = d3.geoPath();
+
+// Data sets
+
+const EDUCATION =
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+const COUNTIES =
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
 
 // Main canvas
 
@@ -19,6 +27,38 @@ const tooltip = d3
   .style('position', 'absolute')
   .style('visibility', 'hidden');
 
-function tooltipTemplate(props) {
-  return `<p>tooltip</p> `;
+// Fetching data
+
+const data = [d3.json(EDUCATION), d3.json(COUNTIES)];
+Promise.all(data).then(render);
+
+// Rendering data
+
+function render([education, counties]) {
+  const county = (d) => education.filter((e) => e.fips === d.id)[0];
+
+  svg
+    .append('g')
+    .selectAll('path')
+    .data(topojson.feature(counties, counties.objects.counties).features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .attr('fill', 'red')
+    .attr('data-fips', (d) => d.id)
+    .attr('data-education', (d) => county(d).bachelorsOrHigher)
+    .attr('class', 'county')
+    .on('mouseover', (e, d) => {
+      const { state, area_name, bachelorsOrHigher } = county(d);
+
+      tooltip
+        .attr('data-education', bachelorsOrHigher)
+        .style('top', e.offsetY - 25 + 'px')
+        .style('left', e.offsetX + 25 + 'px')
+        .style('visibility', 'visible')
+        .html(`<p>${area_name}, ${state}: ${bachelorsOrHigher}%</p>`);
+    })
+    .on('mouseout', () => {
+      tooltip.style('visibility', 'hidden');
+    });
 }
