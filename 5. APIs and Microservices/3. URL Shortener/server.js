c394c31a536fc94f3face1a489d0dd0466600bc0
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-10-08 19:34:13 +0300

    URL Shortener: final version

--- 5. APIs and Microservices/3. URL Shortener/server.js
+++ 5. APIs and Microservices/3. URL Shortener/server.js
@@ -3,33 +3,72 @@
 const express = require('express');
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
+require('dotenv').config();
 
-// Basic Configuration
+const app = express();
 const port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
+
+const urlSchema = new mongoose.Schema({
+  original_url: { type: String, required: true },
+  short_url: {
+    type: String,
+    default: () => Math.random().toString(36).slice(2, 7),
+  },
+});
+
+const URL = mongoose.model('URL', urlSchema);
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', (req, res) => {
+
+app.get('/api', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl/new', (req, res) => {
+  const input = req.body.url;
+
+  const regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+
+  if (!input.match(regex)) {
+    res.json({ error: 'Invalid URL' });
+    return;
+  }
+
+  const shortURL = new URL({ original_url: input });
+  shortURL.save();
+
+  res.json(shortURL);
+});
+
+app.get('/api/shorturl/:query', (req, res) => {
+  URL.findOne({ short_url: req.params.query }, (err, result) => {
+    if (err) {
+      throw new Error(err);
+    }
+    if (!result) {
+      return res.json({ error: 'URL not found' });
+    }
+    res.redirect(result.original_url);
+  });
+});
+
 app.listen(port, () => {
-  console.log('Node.js listening ...');
+  console.log('server listening...');
 });
