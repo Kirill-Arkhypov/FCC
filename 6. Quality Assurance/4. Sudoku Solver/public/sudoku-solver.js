04b5072cf61815e93495062571fed589aebedbec
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-10-16 12:57:38 +0300

    Sudoku Solver: config and package updates

--- 6. Quality Assurance/4. Sudoku Solver/public/sudoku-solver.js
+++ 6. Quality Assurance/4. Sudoku Solver/public/sudoku-solver.js
@@ -3,7 +3,8 @@ const textArea = document.getElementById('text-input');
 
 document.addEventListener('DOMContentLoaded', () => {
   // Load a simple puzzle into the text area
-  textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+  textArea.value =
+    '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
 });
 
 /* 
@@ -12,7 +13,5 @@ document.addEventListener('DOMContentLoaded', () => {
   the client side
 */
 try {
-  module.exports = {
-
-  }
+  module.exports = {};
 } catch (e) {}
--- 6. Quality Assurance/4. Sudoku Solver/tests/2_functional-tests.js
+++ 6. Quality Assurance/4. Sudoku Solver/tests/2_functional-tests.js
@@ -1,15 +1,15 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
-const chai = require("chai");
+const chai = require('chai');
 const assert = chai.assert;
 
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 let Solver;
 
@@ -18,37 +18,32 @@ suite('Functional Tests', () => {
     // DOM already mocked -- load sudoku solver then run tests
     Solver = require('../public/sudoku-solver.js');
   });
-  
-  suite('Text area and sudoku grid update automatically', () => {
-    // Entering a valid number in the text area populates 
-    // the correct cell in the sudoku grid with that number
-    test('Valid number in text area populates correct cell in grid', done => {
 
+  suite('Text area and sudoku grid update automatically', () => {
+    // Entering a valid number in the text area populates
+    // the correct cell in the sudoku grid with that number
+    test('Valid number in text area populates correct cell in grid', (done) => {
       // done();
     });
 
     // Entering a valid number in the grid automatically updates
     // the puzzle string in the text area
-    test('Valid number in grid updates the puzzle string in the text area', done => {
-
+    test('Valid number in grid updates the puzzle string in the text area', (done) => {
       // done();
     });
   });
-  
-  suite('Clear and solve buttons', () => {
-    // Pressing the "Clear" button clears the sudoku 
-    // grid and the text area
-    test('Function clearInput()', done => {
 
+  suite('Clear and solve buttons', () => {
+    // Pressing the "Clear" button clears the sudoku
+    // grid and the text area
+    test('Function clearInput()', (done) => {
       // done();
     });
-    
+
     // Pressing the "Solve" button solves the puzzle and
     // fills in the grid with the solution
-    test('Function showSolution(solve(input))', done => {
-
+    test('Function showSolution(solve(input))', (done) => {
       // done();
     });
   });
 });
-
