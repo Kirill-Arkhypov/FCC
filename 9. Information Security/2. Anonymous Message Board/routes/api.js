df90b425dfea83311e4f297d961919ebd7b35559
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-10-25 14:21:48 +0200

    Anonymous Message Board: database connection

--- 9. Information Security/2. Anonymous Message Board/routes/api.js
+++ 9. Information Security/2. Anonymous Message Board/routes/api.js
@@ -9,8 +9,18 @@
 'use strict';
 
 const expect = require('chai').expect;
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 module.exports = function (app) {
+  mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+
   app.route('/api/threads/:board');
 
   app.route('/api/replies/:board');
