0cf58e35e112d62f1ffd03a75a79dabb9df61461
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-10-26 15:14:07 +0200

    Anonymous Message Board: functionality implemented

--- 9. Information Security/2. Anonymous Message Board/routes/api.js
+++ 9. Information Security/2. Anonymous Message Board/routes/api.js
@@ -9,13 +9,34 @@
 'use strict';
 
 const expect = require('chai').expect;
-const e = require('express');
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const now = () => new Date().toUTCString();
+
+const threadValidator = (threadId, err, thread, res, replyId) => {
+  if (!mongoose.Types.ObjectId.isValid(threadId)) {
+    return res.json('invalid thread id');
+  }
+
+  // console.log( replyId);
+
+  if (replyId) {
+    if (!mongoose.Types.ObjectId.isValid(replyId)) {
+      return res.json('invalid reply id');
+    }
+  }
+
+  if (err) {
+    throw new Error(err);
+  }
+
+  if (!thread) {
+    return res.json('thread not found');
+  }
+};
 
 module.exports = function (app) {
   mongoose.connect(process.env.DB, {
@@ -29,7 +50,6 @@ module.exports = function (app) {
     created_on: {
       type: Date,
       required: true,
-      default: now(),
     },
     reported: { type: Boolean, required: true, default: false },
     delete_password: { type: String, required: true },
@@ -67,32 +87,32 @@ module.exports = function (app) {
       });
     })
     .get((req, res) => {
-      Thread.find({ board: req.params.board }, (err, threads) => {
-        if (err) {
-          throw new Error(err);
-        }
-        const r = threads.map((thread) => ({
-          ...thread._doc,
-          replycount: thread._doc.replies.length,
-        }));
-        return res.json(r);
-      });
-    })
-    .put((req, res) => {
-      Thread.findOneAndUpdate(
-        { _id: req.body.thread_id, board: req.body.board },
-        { bumped_on: now(), reported: true },
-        (err, thread) => {
-          if (!mongoose.Types.ObjectId.isValid(req.body.thread_id)) {
-            return res.json('invalid id');
-          }
-
+      Thread.find({ board: req.params.board })
+        .sort({ bumped_on: 'desc' })
+        .limit(10)
+        .select('-delete_password -reported')
+        .lean()
+        .exec((err, threads) => {
           if (err) {
             throw new Error(err);
           }
-
-          if (!thread) {
-            return res.json('thread not found');
+          const result = threads.map((thread) => ({
+            ...thread,
+            replies: thread.replies
+              .sort((a, b) => b.created_on - a.created_on)
+              .slice(0, 3),
+            replycount: thread.replies.length,
+          }));
+          return res.json(result);
+        });
+    })
+    .put((req, res) => {
+      Thread.findOneAndUpdate(
+        { _id: req.body.thread_id, board: req.body.board || req.params.board },
+        { reported: true },
+        (err, thread) => {
+          if (threadValidator(req.body.thread_id, err, thread, res)) {
+            return;
           }
 
           return res.json('success');
@@ -101,18 +121,10 @@ module.exports = function (app) {
     })
     .delete((req, res) => {
       Thread.findOne(
-        { _id: req.body.thread_id, board: req.body.board },
+        { _id: req.body.thread_id, board: req.body.board || req.params.board },
         async (err, thread) => {
-          if (!mongoose.Types.ObjectId.isValid(req.body.thread_id)) {
-            return res.json('invalid id');
-          }
-
-          if (err) {
-            throw new Error(err);
-          }
-
-          if (!thread) {
-            return res.json('thread not found');
+          if (threadValidator(req.body.thread_id, err, thread, res)) {
+            return;
           }
 
           if (thread.delete_password === req.body.delete_password) {
@@ -125,5 +137,113 @@ module.exports = function (app) {
       );
     });
 
-  app.route('/api/replies/:board');
+  app
+    .route('/api/replies/:board')
+    .post((req, res) => {
+      const newReply = new Reply(req.body);
+      newReply.created_on = now();
+      Thread.findOneAndUpdate(
+        {
+          _id: req.body.thread_id || req.query.thread_id,
+          board: req.body.board || req.params.board,
+        },
+        { $push: { replies: newReply }, bumped_on: now() },
+        { new: true },
+        (err, thread) => {
+          if (threadValidator(req.body.thread_id, err, thread, res)) {
+            return;
+          }
+          return res.redirect(`/b/${thread.board}/${thread._id}`);
+        }
+      );
+    })
+    .get((req, res) => {
+      Thread.findOne({
+        _id: req.body.thread_id || req.query.thread_id,
+        board: req.body.board || req.params.board,
+      })
+        .select('-delete_password -reported')
+        .lean()
+        .exec((err, thread) => {
+          if (err) {
+            throw new Error(err);
+          }
+          const result = {
+            ...thread,
+            replies: thread.replies
+              .map((e) => {
+                return { created_on: e.created_on, _id: e._id, text: e.text };
+              })
+              .sort((a, b) => b.created_on - a.created_on),
+          };
+          return res.json(result);
+        });
+    })
+    .put((req, res) => {
+      Thread.findOne(
+        { _id: req.body.thread_id, board: req.body.board || req.params.board },
+        async (err, thread) => {
+          if (
+            threadValidator(
+              req.body.thread_id,
+              err,
+              thread,
+              res,
+              req.body.reply_id
+            )
+          ) {
+            return;
+          }
+
+          const reply = thread.replies.find(
+            (e) => e._id.toString() === req.body.reply_id
+          );
+
+          if (!reply) {
+            return res.json('reply not found');
+          }
+
+          reply.reported = true;
+
+          await thread.save();
+          return res.json('success');
+        }
+      );
+    })
+    .delete((req, res) => {
+      Thread.findOne(
+        { _id: req.body.thread_id, board: req.body.board || req.params.board },
+        async (err, thread) => {
+          if (
+            threadValidator(
+              req.body.thread_id,
+              err,
+              thread,
+              res,
+              req.body.reply_id
+            )
+          ) {
+            return;
+          }
+
+          const reply = thread.replies.find(
+            (e) => e._id.toString() === req.body.reply_id
+          );
+
+          if (!reply) {
+            return res.json('reply not found');
+          }
+
+          if (reply.delete_password === req.body.delete_password) {
+            thread.bumped_on = now();
+            reply.text = '[deleted]';
+
+            await thread.save();
+            return res.json('success');
+          }
+
+          return res.json('incorrect password');
+        }
+      );
+    });
 };
